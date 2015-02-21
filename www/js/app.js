121d50464e885c87366882d4bc27e2fdd00df475
lamhuy <jason.huy.lam@gmail.com>
2015-02-21 14:20:16 -0500

    ng-facebook

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic', 'app.home'])
+angular.module('starter', ['ionic', 'app.home', 'app.main'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -19,8 +19,6 @@ angular.module('starter', ['ionic', 'app.home'])
 })
 
 
-
-
 .config(function($compileProvider, $stateProvider, $urlRouterProvider, $httpProvider, $ionicConfigProvider) {
 	// Ionic 14
 	$ionicConfigProvider.views.maxCache(0);
@@ -30,42 +28,49 @@ angular.module('starter', ['ionic', 'app.home'])
 	$ionicConfigProvider.backButton.text('');
 
 	// Default to login page
-	$urlRouterProvider.otherwise('/home/');
+	$urlRouterProvider.otherwise('/home');
 
-	$stateProvider.state('home', {		
-		url : '/home/',
-		templateUrl : 'templates/home.html',
-		controller : 'HomeController'		
+	$stateProvider.state('main', {
+		abstract : true,
+		templateUrl : 'templates/main.html',
+		controller : 'MainController'
+	})
+
+	.state('main.home', {		
+		url : '/home',
+		views : {
+			'mainContent' : {
+				templateUrl : 'templates/home.html',
+				controller : 'HomeController'
+			}
+		}
+	})
+	.state('main.about', {		
+		url : '/about',
+		views : {
+			'mainContent' : {
+				templateUrl : 'templates/about.html',
+				controller : 'HomeController'
+			}
+		}
+	})
+	.state('main.portfolio', {		
+		url : '/portfolio',
+		views : {
+			'mainContent' : {
+				templateUrl : 'templates/portfolio.html',
+				controller : 'HomeController'
+			}
+		}
+	})
+	.state('main.contact', {		
+		url : '/contact',
+		views : {
+			'mainContent' : {
+				templateUrl : 'templates/contact.html',
+				controller : 'HomeController'
+			}
+		}
 	})
 	
 });
-
-//
-//.config(function($compileProvider, $stateProvider, $urlRouterProvider, $httpProvider, $ionicConfigProvider) {
-//	// Ionic 14
-//	$ionicConfigProvider.views.maxCache(0);
-//	$ionicConfigProvider.navBar.alignTitle('center');
-//	$ionicConfigProvider.backButton.icon('ion-ios7-arrow-left');
-//	$ionicConfigProvider.backButton.previousTitleText(false);
-//	$ionicConfigProvider.backButton.text('');
-//
-//	// Default to login page
-//	$urlRouterProvider.otherwise('/home/');
-//
-//	$stateProvider.state('main', {
-//		abstract : true,
-//		templateUrl : 'templates/main.html',
-//		controller : 'MainController'
-//	})
-//
-//	.state('main.home', {		
-//		url : '/home/',
-//		views : {
-//			'mainContent' : {
-//				templateUrl : 'templates/home.html',
-//				controller : 'HomeController'
-//			}
-//		}
-//	})
-//	
-//});
